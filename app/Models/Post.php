95faea0f426bdeeff5e4b87226f7e852300df33f
harataisei <harastaisei@gmail.com>
2023-03-18 19:16:04 +0900

    適切なコミットコメント

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'title',
+        'body'];
+    public function getByLimit(int $limit_count = 5)
+{
+    
+    return $this->orderBy('updated_at', 'DESC')->paginate($limit_count);
+}
 }
